38040758a9034fd714ae8f76b12eb83262095944
b0shy <boshyshabani@gmail.com>
2025-03-01 14:42:11 -0500

    Added AWS Lambda function code to my-function.js

---
+++ my-function.js
@@ -0,0 +1,7 @@
+exports.handler = async (event) => {
+	const keyword = event.queryStringParameters?.keyword || 'nothing';
+	return {
+		statusCode: 200,
+		body: JSON:stringify({ message: `Bashar says ${keyword}` }),
+	};
+};
